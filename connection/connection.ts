017038e0dad7559bab03316df4ce4c7bfb273701
Oren Sayag <oren.s@techmarketing.co.il>
2021-11-28 14:26:23 +0200

    fixed bug of scheduling an appointment when there is no slot; Added yargs

--- connection/connection.ts
+++ connection/connection.ts
@@ -4,16 +4,21 @@ import createSlot from "../creators/createSlot";
 import { createTherapist } from "../creators/createTheraspist";
 const mongoose = require('mongoose');
 const faker = require('faker')
+const yargs = require('yargs')
+const argv = yargs.argv
+
 
 main()
 
 async function main() {
 
+    const { accounts, therapists, slots, families } = argv
+
     // TODO this should come from yargs
-    const numberOfAccounts = 2 || 2
-    const numberOfTherapists = 2 || 10
-    const numberOfSlots = 20 || 100
-    const numberOfFamilies = 2 || 2
+    const numberOfAccounts = accounts || 2
+    const numberOfTherapists = therapists || 10
+    const numberOfSlots = slots || 100
+    const numberOfFamilies = families || 2
 
     try {
         await mongoose.connect('mongodb://localhost:27017/test');
@@ -38,6 +43,10 @@ async function main() {
         }
 
         console.log("Done.")
+        console.log(`Created ${accounts || 2} accounts.`)
+        console.log(`${families || 2} families.`)
+        console.log(`${therapists || 10} therapists.`)
+        console.log(`${slots || 100} slots.`)
 
     } catch (error) {
         console.log(error)
--- creators/createAppointment.ts
+++ creators/createAppointment.ts
@@ -1,10 +1,16 @@
 import { Appointments } from "../schemas/appointments"
 import { Slots } from "../schemas/slots"
+import createSlot from "./createSlot"
 const faker = require('faker')
 
 export default async (patientId:string) => {
     let slot = await Slots.find({isAvailable:true})
     slot = slot[0]
+    if(!slot){
+        await createSlot()
+        slot = await Slots.find({isAvailable:true})
+        slot = slot[0]
+    }
     const appointment = new Appointments({
         slot: slot._id,
         linkedToPatient: patientId
